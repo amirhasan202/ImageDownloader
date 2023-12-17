fe03d0173925f0008b22c23c13428c99d8276ee1
amirhasan202 <amirhasan202@gmail.com>
2023-12-17 19:44:54 +0330

    first commit

---
+++ currency-conversion-services/src/test/java/com/in28Minutes/Microservices/currencyconversionservices/CurrencyConversionServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.Microservices.currencyconversionservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
