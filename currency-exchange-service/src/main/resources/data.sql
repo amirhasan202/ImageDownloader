dc8dbef8f5282f6af3059fc6ca711db734abc1d3
amirhasan202 <amirhasan202@gmail.com>
2023-12-17 19:43:44 +0330

    first commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values (10001,'USD','INR',65,'default');
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values (10002,'EUR','INR',80,'default');
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values (10003,'GBP','INR',70,'default');
